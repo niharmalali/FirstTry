bc83c168a8a7308727bf05adb6a060f64b9c1324
niharmalali <blitznihar@hotmail.com>
2016-10-16 19:43:58 -0600

    Revert "COmmitn to new app"
    This reverts commit f65760be5ab50729b2ca904fc71065fa7449faf0.

--- app.js
+++ app.js
@@ -1,36 +1,21 @@
 var express = require('express'),
 mongodbClient = require("mongodb").MongoClient,
-http = require('http'),
 assert = require("assert");
 var app = express();
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.get('/year/:year', function(req, res){
-  //var documents={};
-   console.log("Starting app");
-  mongodbClient.connect("mongodb://localhost:27017/video", function(err, db){
-    assert.equal(null,err);
-    // console.log("Connection Established Successfully!");
-var year = Number(req.params.year);
-console.log(year);
-    db.collection("movies").find({"Year":year}).toArray(function(err,docs){
-      // res.render('data',{objects: docs});
-      // docs.forEach(function(doc){
-      //     console.log(doc.title);
-      // });
-      //res.set('Content-Type','application/json'); //G
-       res.send(docs); //H
-      //res.send("response");
-      // db.close();
 
-    });
-    //resp.send(documents);
 
+console.log("Starting app");
+mongodbClient.connect("mongodb://localhost:27017/video", function(err, db){
+  assert.equal(null,err);
+console.log("Connection Established Successfully!");
+
+db.collection("movies").find({"Year":1990}, function(err,docs){
+
+  docs.forEach(function(doc){
+      console.log(doc.title);
   });
-  // console.log("Called Find");
-});
+  db.close();
 
-http.createServer(app).listen(5000, function() {
-  console.log('Listening on port 5000');
+});
+  console.log("Called Find Successfully!..... Wows! and one more change");
 });
