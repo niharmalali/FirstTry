ffc6a687c46b527088c40fb4b798ccd75d04223a
niharmalali <blitznihar@hotmail.com>
2016-10-16 18:54:27 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express'),
+mongodbClient = require("mongodb").MongoClient,
+assert = require("assert");
+var app = express();
+
+
+console.log("Starting app");
+mongodbClient.connect("mongodb://localhost:27017/video", function(err, db){
+  assert.equal(null,err);
+console.log("Connection Established Successfully!");
+
+db.collection("movies").find({"Year":1990}, function(err,docs){
+
+  docs.forEach(function(doc){
+      console.log(doc.title);
+  });
+  db.close();
+
+});
+  console.log("Called Find");
+});
